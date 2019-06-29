88f966ba1ed3a7d0efc569210b91465b37a14b0d
Jaranchai <jaranchai.nt@hotmail.com>
2019-06-29 22:44:49 +0700

    add use reducer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import BookList from './components/Books/BookLists';
 import SongLists from './components/Songs/SongLists';
 import ThemeToggle from './components/ThemeToggle';
+import Cat from './components/Cat';
 
 import ThemeContextProvider from './contexts/ThemeContext';
 import HookContextProvider from './contexts/HookContext';
@@ -24,6 +25,9 @@ function App() {
         <HookContextProvider>
           <SongLists />
         </HookContextProvider>
+        <hr />
+        <h2>Ract Hook (useReducer)</h2>
+        <Cat />
       </AuthContextProvider>
     </div>
   );
